268e4009c387adde392105ef8dff159f795512db
“Kevin <logikevcoder@gmail.com>
2019-04-17 19:07:13 -0500

    created all necessary actions for the project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAM, FETCH_STREAMS, DELETE_STREAM, EDIT_STREAM } from './types';
 
 export const signIn = userId => {
   return {
@@ -19,4 +19,30 @@ export const createStream = formValues => async dispatch => {
 
   // only care about the data object of the response property
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+// formvalues - some of the edits that are need to be made to update the stream
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  // delete from axios
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
